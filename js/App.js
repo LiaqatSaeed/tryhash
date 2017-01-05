7079fe9d9a0c3962f6928b09a9355c73f9929e14
LiaqatSaeed <liaqatsaeed007@gmail.com>
2017-01-05 14:05:33 +0500

    New files added

---
+++ js/App.js
@@ -0,0 +1,15 @@
+function target() {
+    debugger;
+    $("#target").removeClass("target").addClass("target2");
+}
+
+function loginTo() {
+                debugger;
+                console.log("login button is clicked");
+                //if (window.location.indexOf("mail.google.com") > -1) {
+                //    // Lets login to Gmail
+                //    jQuery("#Email").val("liaqatsaeed007@gmail.com");
+                //    jQuery("#Passwd").val("LIAQAT6777");
+                //    jQuery("#gaia_loginform").submit();
+                //}
+            }
